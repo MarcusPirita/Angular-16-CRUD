7f0b3e812b0c16d5803443ac02d313cc2e588110
MarcusPirita <158137140+MarcusPirita@users.noreply.github.com>
2024-06-01 13:50:02 +0300

    10. video

--- angular-16-crud/creditcardadmin/src/app/creditcards/add/add.component.ts
+++ angular-16-crud/creditcardadmin/src/app/creditcards/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CreditCard } from 'src/app/models/credit-card';
 
 @Component({
   selector: 'app-add',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent {
+
+  newCreditCard: CreditCard = {
+    id: undefined,
+    name: "",
+    description: "",
+    bankName: "",
+    maxCredit: 5000,
+    interestRate: 12,
+    active: true,
+    recommendedScore: "100-500",
+    annualFee: 12,
+    termsAndConditions: "Terms and Conditons for the credit card",
+    createdDate: Date(),
+    updatedDate: Date()
+  }
+
+  saveCreditCard(){
+    console.log("Form Submitted");
+    console.log(this.newCreditCard);
+  }
 
 }
--- angular-16-crud/creditcardadmin/src/app/creditcards/creditcards.module.ts
+++ angular-16-crud/creditcardadmin/src/app/creditcards/creditcards.module.ts
@@ -8,11 +8,19 @@ import { ViewComponent } from './view/view.component';
 import { EditComponent } from './edit/edit.component';
 import { DeleteComponent } from './delete/delete.component';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -29,7 +37,15 @@ import { MatIconModule } from '@angular/material/icon';
     MatTableModule,
     MatPaginatorModule,
     MatCheckboxModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    RouterModule,
+    MatButtonModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class CreditcardsModule { }
